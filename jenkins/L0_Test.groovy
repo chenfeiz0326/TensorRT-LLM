c4cfee73eaca38782fd5f17f7b8b3d397aded586
Chenfei Zhang <chenfeiz@oci-hsg-cs-001-vscode-01.cm.cluster>
2026-03-19 22:27:18 -0700

    update
    Signed-off-by: Chenfei Zhang <chenfeiz@oci-hsg-cs-001-vscode-01.cm.cluster>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1086,10 +1086,7 @@ def runLLMTestlistWithSbatch(pipeline, platform, testList, config=VANILLA_CONFIG
                 envVarsToExport.each { varName, varValue ->
                     srunArgs.add("--container-env=${varName}")
                 }
-
-                println "DEBUG srunArgs before removeAll: ${srunArgs}"
                 srunArgs.removeAll { it == "--mpi=pmi2" || it == "--mpi=pmix" }
-                println "DEBUG srunArgs after removeAll: ${srunArgs}"
 
                 def exemptionComment = ""
                 if (cluster.host.contains("oci-nrt") || cluster.host.contains("oci-hsg") || cluster.host.contains("lbd-lax")) {
